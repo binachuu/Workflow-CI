475545c780fc3f813d991b06870c2d51a9a590af
unknown <tabinar121004@gmail.com>
2026-01-05 05:29:03 +0700

    Fix

--- MLProject/modelling.py
+++ MLProject/modelling.py
@@ -14,23 +14,22 @@ def main():
     mlflow.set_experiment(EXPERIMENT_NAME)
     mlflow.autolog()
 
-    with mlflow.start_run():
+    df = pd.read_csv(DATA_PATH)
+    df[TARGET_COL] = (df[TARGET_COL] > 0).astype(int)
 
-        df = pd.read_csv(DATA_PATH)
-        df[TARGET_COL] = (df[TARGET_COL] > 0).astype(int)
+    X = df.drop(TARGET_COL, axis=1)
+    y = df[TARGET_COL]
 
-        X = df.drop(TARGET_COL, axis=1)
-        y = df[TARGET_COL]
+    X_train, X_test, y_train, y_test = train_test_split(
+        X, y, test_size=0.2, random_state=42, stratify=y
+    )
 
-        X_train, X_test, y_train, y_test = train_test_split(
-            X, y, test_size=0.2, random_state=42, stratify=y
-        )
+    # SATU MODEL SAJA (Basic)
+    model = LogisticRegression(max_iter=1000)
+    model.fit(X_train, y_train)
 
-        model = LogisticRegression(max_iter=1000)
-        model.fit(X_train, y_train)
-
-        acc = accuracy_score(y_test, model.predict(X_test))
-        print("Accuracy:", acc)
+    acc = accuracy_score(y_test, model.predict(X_test))
+    print("Accuracy:", acc)
 
 if __name__ == "__main__":
     main()
